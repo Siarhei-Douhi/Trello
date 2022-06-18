c35517463043c431169da8ae22c4696f5315cf67
Stepan-Bystryk <stewie_the.rofl@mail.ru>
2022-06-18 19:41:52 +0300

    header clock V 1.0

---
+++ src/clock.js
@@ -0,0 +1,7 @@
+export function time() {
+    const clock = document.querySelector('.header_clock');
+    let date = new Date();
+    let hours = date.getHours() < 10 ? '0' + date.getHours() : date.getHours();
+    let minutes = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes();
+    clock.innerHTML = `${hours}:${minutes}`;
+};
--- src/script.js
+++ src/script.js
@@ -1,5 +1,8 @@
 import { app } from "./app.js";
+import { time } from "./clock.js";
 
 window.addEventListener('DOMContentLoaded', () => {
     app();
 });
+
+setInterval(time, 1000);
